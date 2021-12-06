2abc4a3fda4041796066ce45c3c89706d964e5b8
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-12-06 08:18:05 +0100

    replace AnvilScreenHandler.onTakeOutput Redirect with ModifyArg

--- src/main/java/fourmisain/taxfreelevels/mixin/AnvilScreenHandlerMixin.java
+++ src/main/java/fourmisain/taxfreelevels/mixin/AnvilScreenHandlerMixin.java
@@ -8,27 +8,33 @@ import net.minecraft.screen.Property;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-@Mixin(AnvilScreenHandler.class)
+@Mixin(value = AnvilScreenHandler.class, priority = 1500)
 public abstract class AnvilScreenHandlerMixin {
-	/**
-	 * Flatten anvil cost
-	 * Should replace addExperienceLevels(-levelCost)
-	 */
-	@Redirect(
+	@Unique private PlayerEntity taxfreelevels$player;
+
+	@Inject(method = "onTakeOutput", at = @At("HEAD"))
+	public void taxfreelevels$capturePlayer(PlayerEntity player, ItemStack stack, CallbackInfo ci) {
+		taxfreelevels$player = player;
+	}
+
+	@ModifyArg(
 		method = "onTakeOutput",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/entity/player/PlayerEntity;addExperienceLevels(I)V"
-		)
+		),
+		index = 0
 	)
-	public void taxfreelevels$flattenAnvilCost(PlayerEntity player, int negativeLevelCost) {
-		TaxFreeLevels.applyFlattenedAnvilCost(player, -negativeLevelCost);
+	public int taxfreelevels$flattenAnvilCost(int negativeLevelCost) {
+		TaxFreeLevels.applyFlattenedAnvilCost(taxfreelevels$player, -negativeLevelCost);
+		return 0;
 	}
 
 	@Shadow @Final private Property levelCost;
