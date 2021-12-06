475479b372fd482523b1e1f22f2074bcb0a1f306
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-12-06 13:51:28 +0100

    - replace Redirect with ModifyArg inside RerollMixin - only target Reroll's "reroll" method (versions 1.2.1 and up) - set remap=true in At (remap is inherited by the upper annotation otherwise)

--- src/main/java/io/github/fourmisain/taxfreelevels/mixin/RerollMixin.java
+++ src/main/java/io/github/fourmisain/taxfreelevels/mixin/RerollMixin.java
@@ -4,24 +4,34 @@ import draylar.reroll.Reroll;
 import io.github.fourmisain.taxfreelevels.TaxFreeLevels;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Reroll.class)
+@Mixin(value = Reroll.class, priority = 1500)
 public abstract class RerollMixin {
-	/**
-	 * Flatten reroll cost
-	 * Should replace PlayerEntity.addExperienceLevels(-levelsPerReroll)
-	 */
-	@Redirect(
-		method = {"lambda$null$2", "reroll"},
+	@Unique
+	private static PlayerEntity taxfreelevels$player;
+
+	@Inject(method = "reroll", at = @At("HEAD"))
+	private static void taxfreelevels$capturePlayer(PlayerEntity player, CallbackInfo ci) {
+		taxfreelevels$player = player;
+	}
+
+	@ModifyArg(
+		method = "reroll",
 		at = @At(
 			value = "INVOKE",
-			target = "Lnet/minecraft/class_1657;method_7316(I)V"
+			target = "Lnet/minecraft/entity/player/PlayerEntity;addExperienceLevels(I)V",
+			remap = true
 		),
+		index = 0,
 		remap = false
 	)
-	private static void taxfreelevels$flattenRerollCost(PlayerEntity player, int negativeLevelCost) {
-		TaxFreeLevels.applyFlattenedEnchantmentCost(player, -negativeLevelCost);
+	private static int taxfreelevels$flattenRerollCost(int negativeLevelCost) {
+		TaxFreeLevels.applyFlattenedEnchantmentCost(taxfreelevels$player, -negativeLevelCost);
+		return 0;
 	}
 }
