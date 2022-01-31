45dbebe9ebafa516c8c24b4f0e89b55773b7e296
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2022-01-31 17:23:28 +0100

    - move all common mixins to fabric/forge due to mixin plugin complications - disable fabric (standard build is preferable at this point) - disable anvil mixins when Enchanting Overhauled is found

---
+++ forge/src/main/java/io/github/fourmisain/taxfreelevels/mixin/forge/CheapAnvilRenameMixin.java
@@ -0,0 +1,36 @@
+package io.github.fourmisain.taxfreelevels.mixin.forge;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.AnvilScreenHandler;
+import net.minecraft.screen.Property;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(value = AnvilScreenHandler.class)
+public abstract class CheapAnvilRenameMixin {
+	@Shadow @Final private Property levelCost;
+
+	/**
+	 * Make item renaming always cost 1 level
+	 * This should inject after levelCost.set(j + i) and levelCost.set(39)
+	 */
+	@Inject(method = "updateResult",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/screen/Property;get()I",
+			ordinal = 1
+		),
+		locals = LocalCapture.CAPTURE_FAILHARD)
+	public void taxfreelevels$makeRenamingCheap(CallbackInfo ci, ItemStack itemStack, int i, int j, int k) {
+		// k is set to 1 when renaming, i is the total cost without the repair cost j,
+		// so this condition means we are only renaming:
+		if (k > 0 && k == i) {
+			levelCost.set(1);
+		}
+	}
+}
