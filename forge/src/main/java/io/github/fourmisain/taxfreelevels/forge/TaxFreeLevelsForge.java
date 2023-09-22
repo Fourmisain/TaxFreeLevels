e97c4fbb78eee9109a51479c8fc1b89e80ea61b7
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-09-22 11:49:44 +0200

    update to 1.20.2

--- forge/src/main/java/io/github/fourmisain/taxfreelevels/forge/TaxFreeLevelsForge.java
+++ forge/src/main/java/io/github/fourmisain/taxfreelevels/forge/TaxFreeLevelsForge.java
@@ -4,13 +4,12 @@ import io.github.fourmisain.taxfreelevels.TaxFreeLevels;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(TaxFreeLevels.MOD_ID)
 public class TaxFreeLevelsForge {
 	public TaxFreeLevelsForge() {
 		// Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
 		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
-			() -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+			() -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
 	}
 }
