2953562b84d452bbd3811d4715e8e05d8e987798
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2023-10-16 17:30:38 +0200

    use extension point api call for older forge versions (now compatible from 1.17 to 1.20.2)

--- forge/src/main/java/io/github/fourmisain/taxfreelevels/forge/TaxFreeLevelsForge.java
+++ forge/src/main/java/io/github/fourmisain/taxfreelevels/forge/TaxFreeLevelsForge.java
@@ -2,14 +2,20 @@ package io.github.fourmisain.taxfreelevels.forge;
 
 import io.github.fourmisain.taxfreelevels.TaxFreeLevels;
 import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 @Mod(TaxFreeLevels.MOD_ID)
 public class TaxFreeLevelsForge {
+	private static final String IGNORESERVERONLY = "OHNOES\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31\uD83D\uDE31";
+
 	public TaxFreeLevelsForge() {
-		// Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
-			() -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		if (ModList.get().getModContainerById("forge").get().getModInfo().getVersion().compareTo(new DefaultArtifactVersion("41.0.15")) < 0) {
+			// Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+			ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
+				() -> new IExtensionPoint.DisplayTest(() -> IGNORESERVERONLY, (a, b) -> true));
+		}
 	}
 }
