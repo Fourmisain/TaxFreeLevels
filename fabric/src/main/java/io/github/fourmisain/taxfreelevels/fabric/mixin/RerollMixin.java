17df61bb9610c6b41b32f2a73ccb4b7741a36260
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2024-10-16 20:28:11 +0200

    respect level requirement for rerolling

--- fabric/src/main/java/io/github/fourmisain/taxfreelevels/fabric/mixin/RerollMixin.java
+++ fabric/src/main/java/io/github/fourmisain/taxfreelevels/fabric/mixin/RerollMixin.java
@@ -4,12 +4,23 @@ import com.llamalad7.mixinextras.sugar.Local;
 import draylar.reroll.Reroll;
 import io.github.fourmisain.taxfreelevels.TaxFreeLevels;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.EnchantmentScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = Reroll.class, priority = 1500)
+@Mixin(value = Reroll.class, priority = 1500, remap = false)
 public abstract class RerollMixin {
+	@Inject(method = "reroll", at = @At(value = "INVOKE", target = "Ldraylar/reroll/impl/PlayerEntityManipulator;rerollEnchantmentSeed()V"), remap = false)
+	private static void taxfreelevels$setLevelRequirement(CallbackInfo ci, @Local(argsOnly = true) PlayerEntity player) {
+		if (player.currentScreenHandler instanceof EnchantmentScreenHandler enchantmentScreenHandler) {
+			// you would normally "reroll" by using the cheapest option
+			TaxFreeLevels.setLevelRequirement(enchantmentScreenHandler.enchantmentPower[0]);
+		}
+	}
+
 	@ModifyArg(
 		method = "reroll",
 		at = @At(
@@ -23,5 +34,10 @@ public abstract class RerollMixin {
 	private static int taxfreelevels$flattenRerollCost(int negativeLevelCost, @Local(argsOnly = true) PlayerEntity player) {
 		TaxFreeLevels.applyFlattenedXpCost(player, -negativeLevelCost);
 		return 0; // we already paid in XP
+	}
+
+	@Inject(method = "reroll", at = @At("TAIL"), remap = false)
+	private static void taxfreelevels$resetLevelRequirement(CallbackInfo ci) {
+		TaxFreeLevels.resetLevelRequirement();
 	}
 }
