614c2b22f5a2bf8a92a0d92edb17817820c8c8fd
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2025-03-28 09:13:47 +0100

    more stable RemoveAnvilLimitMixin (fix crash on neoforge 21.5.1-beta and up)

--- common/src/main/java/io/github/fourmisain/taxfreelevels/mixin/RemoveAnvilLimitMixin.java
+++ common/src/main/java/io/github/fourmisain/taxfreelevels/mixin/RemoveAnvilLimitMixin.java
@@ -5,12 +5,21 @@ import io.github.fourmisain.taxfreelevels.TaxFreeLevelsConfig;
 import net.minecraft.screen.AnvilScreenHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @SuppressWarnings("SimplifiableConditionalExpression")
 @Mixin(AnvilScreenHandler.class)
 public abstract class RemoveAnvilLimitMixin {
 	// mods may ModifyConstant the level 40 limit, so for compatibility we change isInCreativeMode() instead
-	@ModifyExpressionValue(method = "updateResult", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isInCreativeMode()Z", ordinal = 1))
+	@ModifyExpressionValue(
+		method = "updateResult",
+		slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/screen/Property;get()I", ordinal = 0)), // levelCost.get()
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/entity/player/PlayerEntity;isInCreativeMode()Z",
+			ordinal = 0
+		)
+	)
 	public boolean taxfreelevels$removeAnvilLimit(boolean original) {
 		return TaxFreeLevelsConfig.get().removeAnvilLimit ? true : original;
 	}
