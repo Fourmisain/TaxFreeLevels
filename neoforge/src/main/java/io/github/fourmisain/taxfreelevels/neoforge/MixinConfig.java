b5a8605cb6f59addfa72e259100059b9b5d10ebc
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2026-08-26 12:39:03 +0200

    compatibility with Quark's Matrix Enchanting

--- neoforge/src/main/java/io/github/fourmisain/taxfreelevels/neoforge/MixinConfig.java
+++ neoforge/src/main/java/io/github/fourmisain/taxfreelevels/neoforge/MixinConfig.java
@@ -76,6 +76,9 @@ public class MixinConfig implements IMixinConfigPlugin {
 
 		if (!isModInstalled("sophisticatedbackpacks"))
 			disabledMixins.add("sophisticatedbackpacks.AnvilUpgradeTabMixin");
+
+		if (!isModInstalled("quark"))
+			disabledMixins.add("quark.MatrixEnchantingTableBlockEntity");
 	}
 
 	@Override
---
+++ neoforge/src/main/java/io/github/fourmisain/taxfreelevels/neoforge/mixin/quark/MatrixEnchantingTableBlockEntityMixin.java
@@ -0,0 +1,42 @@
+package io.github.fourmisain.taxfreelevels.neoforge.mixin.quark;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import io.github.fourmisain.taxfreelevels.TaxFreeLevels;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.violetmoon.quark.addons.oddities.block.be.MatrixEnchantingTableBlockEntity;
+import org.violetmoon.quark.addons.oddities.inventory.EnchantmentMatrix;
+
+@Mixin(MatrixEnchantingTableBlockEntity.class)
+public abstract class MatrixEnchantingTableBlockEntityMixin {
+	@Shadow
+	public int bookshelfPower;
+
+	@Inject(method = "generateAndPay", at = @At("HEAD"))
+	public void taxfreelvels$setLevelRequirement(EnchantmentMatrix matrix, PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+		TaxFreeLevels.setLevelRequirement(matrix.getMinXpLevel(bookshelfPower));
+	}
+
+	@Inject(method = "generateAndPay", at = @At("RETURN"))
+	public void taxfreelvels$resetLevelRequirement(EnchantmentMatrix matrix, PlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
+		TaxFreeLevels.resetLevelRequirement();
+	}
+
+	@ModifyArg(
+		method = "generateAndPay",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/entity/player/PlayerEntity;addExperienceLevels(I)V"
+		),
+		index = 0
+	)
+	private int taxfreelevels$flattenRepairCost(int negativeLevelCost, @Local(argsOnly = true) PlayerEntity player) {
+		TaxFreeLevels.applyFlattenedXpCost(player, -negativeLevelCost);
+		return 0; // we already paid in XP
+	}
+}
